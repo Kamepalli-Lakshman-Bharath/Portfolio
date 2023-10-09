276b9dac1526eebc664da88230ef8283d942a47e
Kamepalli-Lakshman-Bharath <126488003+Kamepalli-Lakshman-Bharath@users.noreply.github.com>
2023-10-09 13:58:21 +0530

    Update page.js

--- src/app/(route)/Certificates/page.js
+++ src/app/(route)/Certificates/page.js
@@ -66,23 +66,47 @@ export default function Certificates() {
 
       <div className={styles.card}>
         <p>
-          <span className={styles.title}>Name</span>: Job Focussed Professional
-          Communication Certificate.
+          <span className={styles.title}>Name</span>: MongoDB Express and Node.js (MEN) Certification
         </p>
         <p>
-          <span className={styles.title}>Skills</span>: React js, React Redux
-          Toolkit.
+          <span className={styles.title}>Skills</span>: MongoDB, Express and Node.js.
         </p>
         <p>
           <span className={styles.title}>Issued by</span>: EDYODA DIGITAL
           UNIVERSITY.
         </p>
         <p>
-          <span className={styles.title}>Description</span>: I possess a Job-Focused
-          Professional Communication Certificate, emphasizing my strong
-          communication skills and readiness for the professional world. This
-          certification underscores my ability to effectively convey ideas and
-          collaborate within the workplace.
+          <span className={styles.title}>Description</span>: Demonstrated proficiency in developing and deploying web applications using MongoDB, Express, and Node.js. Expertise in RESTful API design, authentication, and authorization.
+        </p>
+        <p>
+          <a
+            target="_blank"
+            className={styles.link}
+            href="https://edyodalms.s3.amazonaws.com/files/certificates/Lakshman__Bharath_achivement_Node_Express_Mongo_Certification_16292_Qr_code_.png"
+          >
+            Click here
+          </a>
+        </p>
+      </div>
+
+      <div className={styles.card}>
+        <p>
+          <span className={styles.title}>Name</span>: Job Focussed Professional
+          Communication Certificate.
+        </p>
+        <p>
+          <span className={styles.title}>Skills</span>: Communication, Team Mangement, LeaderShip Skills
+        </p>
+        <p>
+          <span className={styles.title}>Issued by</span>: EDYODA DIGITAL
+          UNIVERSITY.
+        </p>
+        <p>
+          <span className={styles.title}>Description</span>: I possess a
+          Job-Focused Professional Communication Certificate, emphasizing my
+          strong communication skills and readiness for the professional world.
+          This certification underscores my ability to effectively convey ideas
+          and collaborate within the workplace.
         </p>
         <p>
           <a
